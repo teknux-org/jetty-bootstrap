aaa6d947217529a2108747afd87de0098c559d18
Francois EYL <sma.feyl@gmail.com>
2013-12-18 12:04:29 +0100

    Provide support for Jetty Configuration from properties (custom and system). Add related unit tests.

---
+++ src/main/java/org/genux/jettybootstrap/utils/PropertiesUtils.java
@@ -0,0 +1,89 @@
+package org.genux.jettybootstrap.utils;
+
+import java.util.Properties;
+
+
+/**
+ * Utility class to easy {@link Properties} handling.
+ * 
+ * @author "Francois EYL"
+ */
+public class PropertiesUtils {
+
+	private PropertiesUtils() {
+	}
+
+	/**
+	 * @param p
+	 * @param key
+	 * @return the {@link Long} or <code>null</code>
+	 */
+	public static Long parseLong(Properties p, String key) {
+		if (p == null || key == null) {
+			return null;
+		}
+
+		String value = p.getProperty(key);
+		if (value == null) {
+			return null;
+		}
+
+		return Long.parseLong(value);
+	}
+
+	/**
+	 * @param p
+	 * @param key
+	 * @return {@link Integer} or <code>null</code>
+	 */
+	public static Integer parseInt(Properties p, String key) {
+		if (p == null || key == null) {
+			return null;
+		}
+
+		String value = p.getProperty(key);
+		if (value == null) {
+			return null;
+		}
+
+		return Integer.parseInt(value);
+	}
+
+	/**
+	 * @param p
+	 * @param key
+	 * @param separator
+	 * @return an array of {@link String} or
+	 *         <code>null</code>
+	 */
+	public static String[] parseArray(Properties p, String key, String separator) {
+		if (p == null || key == null || separator == null || separator.isEmpty()) {
+			return null;
+		}
+
+		String value = p.getProperty(key);
+		if (value == null) {
+			return null;
+		}
+
+		return value.contains(separator) ? value.split(separator) : new String[] { value };
+	}
+
+	/**
+	 * @param p
+	 * @param key
+	 * @return {@link Boolean} or <code>null</code>
+	 */
+	public static Boolean parseBoolean(Properties p, String key) {
+		if (p == null || key == null) {
+			return null;
+		}
+
+		String value = p.getProperty(key);
+		if (value == null) {
+			return null;
+		}
+
+		return Boolean.parseBoolean(value);
+	}
+}
--- src/test/java/org/genux/jettybootstrap/test/configuration/PropertiesJettyConfigurationTest.java
+++ src/test/java/org/genux/jettybootstrap/test/configuration/PropertiesJettyConfigurationTest.java
@@ -1,32 +1,114 @@
 package org.genux.jettybootstrap.test.configuration;
 
+import java.io.File;
 import java.util.Properties;
 
+import org.genux.jettybootstrap.configuration.JettyConnector;
 import org.genux.jettybootstrap.configuration.PropertiesJettyConfiguration;
 import org.junit.Assert;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 
 public class PropertiesJettyConfigurationTest {
 
-	private static Properties testValuesProperties;
-
-	@BeforeClass
-	public static void init() {
-		testValuesProperties = new Properties();
-
-		testValuesProperties.setProperty(PropertiesJettyConfiguration.KEY_AUTO_JOIN_ON_START, "0");
-		testValuesProperties.setProperty(PropertiesJettyConfiguration.KEY_CONNECTORS, "C1,C2");
+	@Test
+	public void parseJettyConnetorsTest() {
+		Assert.assertArrayEquals(null, PropertiesJettyConfiguration.parseConnectors(new String[] { "ef", "SeSL" }));
+		Assert.assertArrayEquals(new JettyConnector[] { JettyConnector.SSL }, PropertiesJettyConfiguration.parseConnectors(new String[] { "ef", "SSL" }));
+		Assert.assertArrayEquals(new JettyConnector[] { JettyConnector.DEFAULT }, PropertiesJettyConfiguration.parseConnectors(new String[] { "DEFAULT" }));
+		Assert.assertArrayEquals(new JettyConnector[] { JettyConnector.DEFAULT, JettyConnector.SSL },
+				PropertiesJettyConfiguration.parseConnectors(new String[] { "DEFAULT", "SSL" }));
 	}
 
 	@Test
-	public void parseIntTest() {
-		Assert.assertNull(PropertiesJettyConfiguration.parseInt(null, "test"));
-		Assert.assertNull(PropertiesJettyConfiguration.parseInt(null, null));
-		Assert.assertNull(PropertiesJettyConfiguration.parseInt(new Properties(), null));
+	public void systemPropertiesTest() {
+		//system properties
+		System.setProperty(PropertiesJettyConfiguration.KEY_AUTO_JOIN_ON_START, "true");
+		System.setProperty(PropertiesJettyConfiguration.KEY_MAX_THREADS, "1");
+		System.setProperty(PropertiesJettyConfiguration.KEY_STOP_AT_SHUTDOWN, "false");
+		System.setProperty(PropertiesJettyConfiguration.KEY_STOP_TIMEOUT, "2");
+		System.setProperty(PropertiesJettyConfiguration.KEY_IDLE_TIMEOUT, "3");
+		System.setProperty(PropertiesJettyConfiguration.KEY_HOST, "localhost1");
+		System.setProperty(PropertiesJettyConfiguration.KEY_PORT, "4444");
+		System.setProperty(PropertiesJettyConfiguration.KEY_SSL_PORT, "5555");
+		System.setProperty(PropertiesJettyConfiguration.KEY_CONNECTORS, "SSL");
+		System.setProperty(PropertiesJettyConfiguration.KEY_REDIRECT_ALL_ON_SSL, "false");
+		System.setProperty(PropertiesJettyConfiguration.KEY_SSL_KEYSTORE_PASSWORD, "pwd1");
+		System.setProperty(PropertiesJettyConfiguration.KEY_SSL_KEYSTORE_PATH, "./keystore1");
+		System.setProperty(PropertiesJettyConfiguration.KEY_TEMP_DIR, "/tmp1");
+		System.setProperty(PropertiesJettyConfiguration.KEY_PARENT_LOADER_PRIORITY, "true");
 
-		Assert.assertEquals(new Integer(0), PropertiesJettyConfiguration.parseInt(testValuesProperties, PropertiesJettyConfiguration.KEY_AUTO_JOIN_ON_START));
-		Assert.assertArrayEquals(new String[] { "C1", "C2" }, PropertiesJettyConfiguration.parseArray(testValuesProperties, PropertiesJettyConfiguration.KEY_CONNECTORS, ","));
+		//test sys prop config only
+		PropertiesJettyConfiguration cfg = new PropertiesJettyConfiguration();
+		Assert.assertEquals(true, cfg.isAutoJoinOnStart());
+		Assert.assertEquals(1, cfg.getMaxThreads());
+		Assert.assertEquals(false, cfg.isStopAtShutdown());
+		Assert.assertEquals(2L, cfg.getStopTimeout());
+		Assert.assertEquals(3L, cfg.getIdleTimeout());
+		Assert.assertEquals("localhost1", cfg.getHost());
+		Assert.assertEquals(4444, cfg.getPort());
+		Assert.assertEquals(5555, cfg.getSslPort());
+		Assert.assertTrue(cfg.getJettyConnectors().size() == 1);
+		Assert.assertTrue(cfg.getJettyConnectors().contains(JettyConnector.SSL));
+		Assert.assertEquals(false, cfg.isRedirectAllOnSslConnector());
+		Assert.assertEquals("pwd1", cfg.getSSLKeyStorePassword());
+		Assert.assertEquals("./keystore1", cfg.getSSLKeyStorePath());
+		Assert.assertEquals(new File("/tmp1"), cfg.getTempDirectory());
+		Assert.assertEquals(true, cfg.getParentLoaderPriority());
+
+		//custom properties
+		final Properties properties = new Properties();
+		properties.setProperty(PropertiesJettyConfiguration.KEY_AUTO_JOIN_ON_START, "false");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_MAX_THREADS, "11");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_STOP_AT_SHUTDOWN, "true");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_STOP_TIMEOUT, "22");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_IDLE_TIMEOUT, "33");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_HOST, "localhost11");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_PORT, "8080");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_SSL_PORT, "8443");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_CONNECTORS, "DEFAULT,SSL");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_REDIRECT_ALL_ON_SSL, "true");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_SSL_KEYSTORE_PASSWORD, "pwd2");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_SSL_KEYSTORE_PATH, "./keystore2");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_TEMP_DIR, "/tmp2");
+		properties.setProperty(PropertiesJettyConfiguration.KEY_PARENT_LOADER_PRIORITY, "false");
+
+		//test given prop config only
+		cfg = new PropertiesJettyConfiguration(properties, true);
+		Assert.assertEquals(false, cfg.isAutoJoinOnStart());
+		Assert.assertEquals(11, cfg.getMaxThreads());
+		Assert.assertEquals(true, cfg.isStopAtShutdown());
+		Assert.assertEquals(22L, cfg.getStopTimeout());
+		Assert.assertEquals(33L, cfg.getIdleTimeout());
+		Assert.assertEquals("localhost11", cfg.getHost());
+		Assert.assertEquals(8080, cfg.getPort());
+		Assert.assertEquals(8443, cfg.getSslPort());
+		Assert.assertTrue(cfg.getJettyConnectors().size() == 2);
+		Assert.assertTrue(cfg.getJettyConnectors().contains(JettyConnector.SSL));
+		Assert.assertTrue(cfg.getJettyConnectors().contains(JettyConnector.DEFAULT));
+		Assert.assertEquals(true, cfg.isRedirectAllOnSslConnector());
+		Assert.assertEquals("pwd2", cfg.getSSLKeyStorePassword());
+		Assert.assertEquals("./keystore2", cfg.getSSLKeyStorePath());
+		Assert.assertEquals(new File("/tmp2"), cfg.getTempDirectory());
+		Assert.assertEquals(false, cfg.getParentLoaderPriority());
+
+		//test sys prop and custom config with system having higher priority
+		cfg = new PropertiesJettyConfiguration(properties);
+		Assert.assertEquals(true, cfg.isAutoJoinOnStart());
+		Assert.assertEquals(1, cfg.getMaxThreads());
+		Assert.assertEquals(false, cfg.isStopAtShutdown());
+		Assert.assertEquals(2L, cfg.getStopTimeout());
+		Assert.assertEquals(3L, cfg.getIdleTimeout());
+		Assert.assertEquals("localhost1", cfg.getHost());
+		Assert.assertEquals(4444, cfg.getPort());
+		Assert.assertEquals(5555, cfg.getSslPort());
+		Assert.assertTrue(cfg.getJettyConnectors().size() == 1);
+		Assert.assertTrue(cfg.getJettyConnectors().contains(JettyConnector.SSL));
+		Assert.assertEquals(false, cfg.isRedirectAllOnSslConnector());
+		Assert.assertEquals("pwd1", cfg.getSSLKeyStorePassword());
+		Assert.assertEquals("./keystore1", cfg.getSSLKeyStorePath());
+		Assert.assertEquals(new File("/tmp1"), cfg.getTempDirectory());
+		Assert.assertEquals(true, cfg.getParentLoaderPriority());
 	}
+
 }
